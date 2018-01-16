f8cca502f09bc6048dbf9bd8f1efad49b1cf2d8f
Djalma  Bina <djbina2000@yahoo.com.br>
2018-01-16 12:08:09 -0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello!You  are  connected  in  my  Heroku Center Applications please  enjoy them') 
 })
 
 app.listen(app.get('port'), function() {
